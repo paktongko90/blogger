857fbc96dcb7afd1f379dff622fb7bdfb5cbc730
Faizul Bin. Baba <faizul@iris.com.my>
2022-06-15 14:59:30 +0800

    first commit

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Http\Requests\RegisterRequest;
+
+class RegisterController extends Controller
+{
+    /**
+     * Display register page.
+     * 
+     * @return \Illuminate\Http\Response
+     */
+
+    public function show(){
+        return view('auth.register');
+    }
+
+    /**
+     * Handle account registration request
+     * 
+     * @param RegisterRequest $request
+     * 
+     * @return \Illuminate\Http\Response
+     */
+
+    public function register(RegisterRequest $request){
+        
+    }
+}
